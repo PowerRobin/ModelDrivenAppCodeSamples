95396e4217ccf1817fd5d76170aefc3acf88c06b
Robin <70424413+PowerRobin@users.noreply.github.com>
2024-02-17 23:32:31 +0100

    Less parameters, More comfort

--- CustomPageAsDialogue/openPage.js
+++ CustomPageAsDialogue/openPage.js
@@ -1,5 +1,14 @@
 //just one parameter separated with ; like this: title;name;entityName;target;position;width;width_unit;height;height_unit
-//example string to pass as the page_definition parameter: My Title;r2p_mypage_e56f0;r2p_mytable;2;1;700;px;500;px
+
+// Title: Title that appears on the MDA
+// Pagename: logical name of the page
+// Additional parameter: pass whatever you want - you can retrieve it with Param(entityName) in the custom page
+// Target/Position (optional, defaults to side): main,center,side
+// Width (optional, defaults to 300px): width in px or % (500px/30%)
+// Height (optional, defaults to 100%): width in px or % (500px/30%)
+
+//example string to pass as the page_definition parameter (opens a center dialog): My Title;r2p_mypage_e56f0;additional parameter;center;700px;500px
+//example string to pass as the page_definition parameter (opens a 300px wide side dialog): My Title;r2p_mypage_e56f0;additional parameter
 
 function openPage(record, page_definition) {
 
@@ -10,12 +19,12 @@ function openPage(record, page_definition) {
 	var title = valuesArray[0];
 	var name = valuesArray[1];
 	var entityName = valuesArray[2]; //hint: you don't actually need the entity name! you could use it to pass other parameters to the custom page
-	var target = parseInt(valuesArray[3]);
-	var position = parseInt(valuesArray[4]);
-	var width = parseInt(valuesArray[5]);
-	var width_unit = valuesArray[6];
-	var height = parseInt(valuesArray[7]);
-	var height_unit = valuesArray[8];
+	var target; if (valuesArray[3] === 'main') {target = 1} else {target = 2};
+	var position; if (valuesArray[3] === 'center') {position = 1} else {position = 2};
+  var width = valuesArray[4] ? parseInt(valuesArray[4]) : 300;
+  var width_unit = valuesArray[4] ? (valuesArray[4].slice(-1) === "%" ? "%" : "px") : "px";
+	var height = valuesArray[5] ? parseInt(valuesArray[5]) : 100;
+  var height_unit = valuesArray[5] ? (valuesArray[5].slice(-2) === "px" ? "px" : "%") : "%";
 	var recordId = record.replace(/\{|\}/g, ''); //removes the curly braces, so you don't have to in the Canvas App
 
 	//save the form before opening the page
